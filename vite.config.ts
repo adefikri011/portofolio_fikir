b207020b36e282b4ee4caf93cc7ffc75c67c8fd3
adefikri011 <fikriade257@gmail.com>
2025-11-04 22:53:48 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // ✅ gunakan base relatif untuk Vercel
+  base: './',
   plugins: [react()],
 })
